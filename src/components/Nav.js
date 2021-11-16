170c9e510e9a04ce631bc4582eb1bd1c42aac98b
Rayan84 <rayan_84@live.com>
2021-11-16 14:22:26 +0100

    Fix eslint and stylelint issues

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,15 +8,15 @@ import {
 import Rockets from './Rockets';
 
 const Nav = () => (
-    <Router>
-      <ul>
-        <li><Link to="/">Rockets</Link></li>
-        <li />
-      </ul>
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-      </Routes>
-    </Router>
-  );
+  <Router>
+    <ul>
+      <li><Link to="/">Rockets</Link></li>
+      <li />
+    </ul>
+  <Routes>
+    <Route path="/" element={<Rockets />} />
+    </Routes>
+  </Router>
+);
 
 export default Nav;
