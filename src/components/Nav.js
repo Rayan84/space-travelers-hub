15869172013520e90ef91e819ecd8aa54d17bd11
Rayan84 <rayan_84@live.com>
2021-11-17 11:53:27 +0100

    Fix eslint

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,36 +6,35 @@ import {
   Route,
 } from 'react-router-dom';
 import Rockets from './Rockets';
-import Reservations from './Reservations';
+import Missions from './Missions';
 
 const Nav = () => {
   function linkStyle(num) {
-    let links = document.querySelectorAll('.nav-link');   
-    for (let i = 0; i < links.length; i++){
-      if(i !== num){
+    const links = document.querySelectorAll('.nav-link');
+    for (let i = 0; i < links.length; i+= 1) {
+      if (i !== num) {
         links[i].style.textDecoration = 'none';
-      }else{
+      } else {
         links[i].style.textDecoration = 'underline';
       }
     }
-  };
+  }
 
-return(
-  <Router>
-    <>
-    <ul className="navbar">
-      <li><NavLink to="/rockets" className="nav-link" onClick={() => {linkStyle(0)}}>Rockets</NavLink></li>
-      <li><NavLink to="/reservations" className="nav-link" onClick={() => {linkStyle(1)}}>Reservations</NavLink></li>
-    </ul>
-    </>
-    <Routes>
-      <Route path="/rockets" element={<Rockets />} />
-      <Route path="/reservations" element={<Reservations />} />
-      <Route path="/" element={<Rockets />} />
-
-    </Routes>
-  </Router>
-);
+  return (
+    <Router>
+      <>
+        <ul className="navbar">
+          <li><NavLink to="/rockets" className="nav-link" onClick={ () => {linkStyle(0);} }>Rockets</NavLink></li>
+          <li><NavLink to="/missions" className="nav-link" onClick={ () => {linkStyle(1);} }>Missions</NavLink></li>
+        </ul>
+      </>
+      <Routes>
+        <Route path="/rockets" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/" element={<Rockets />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default Nav;
