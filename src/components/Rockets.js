1d5b18352ddf54397fa1770f1d247223ce699f43
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-11-19 12:14:12 +0300

    fetch rocket data

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -15,13 +15,14 @@ const Rockets = () => {
           <li key={rocket.id}>
             <p>
               {' '}
-              {rocket.rocket_id}
+              {rocket.id}
             </p>
 
             <h2>{rocket.rocket_name}</h2>
             <p>
               <strong>{rocket.description}</strong>
             </p>
+            <img src={rocket.flickr_images} alt="" width="80" height="80" />
             <button
               onClick={() => {
                 dispatch(fetchRockets());
